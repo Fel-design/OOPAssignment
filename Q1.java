928eceea5bd7f7e2f05b3cc62c0d4739e592a475
Fel-design <felistanjeri2005@gmail.com>
2025-01-23 19:02:13 +0300

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,15 @@
+// This is a simple Java program to print "Hello, World!"
+
+// Define the HelloWorld class
+public class HelloWorld {
+
+    // The main method: This is the entry point of the program where execution starts
+    public static void main(String[] args) {
+
+        // The System.out.println method prints the specified string to the console
+        // "System" is a built-in Java class that contains several useful methods and fields
+        // "out" is a static field of the System class, which is an instance of the PrintStream class
+        // "println" is a method of the PrintStream class that prints a line of text to the console
+        System.out.println("Hello, World!");
+    }
+}
